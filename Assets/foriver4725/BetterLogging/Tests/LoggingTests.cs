e6a5624f7c95391aa6ad2569437daec5bcc15a2c
foriver4725 <hrt67@icloud.com>
2025-11-05 21:07:43 +0900

    Added tests

---
+++ Assets/foriver4725/BetterLogging/Tests/LoggingTests.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace foriver4725.BetterLogging.Tests
+{
+    public static class LoggingTests
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private static void Init()
+        {
+            "Hello, World!".Print();
+            "This is a warning message.".Print(LogSettings.Warning);
+            "This is an error message.".Print(LogSettings.Error);
+            "This is a custom colored message.".Print(
+                LogSettings.Level_Warning | LogSettings.Color_Hue_Cyan | LogSettings.Color_Tone_Light);
+            "This is a custom colored message with override color.".Print(LogSettings.Level_Normal, Color.magenta);
+        }
+    }
+}
